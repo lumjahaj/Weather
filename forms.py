b8f1f86751f66bae539c12431069505f63fc94b8
Lum Jahaj <lumjahaj@live.com>
2025-07-08 18:52:49 +0200

    Started recreating the app

--- forms.py
+++
@@ -1,5 +0,0 @@
-
-class WeatherForm(FlaskForm):
-    city = StringField('City', validators=[DataRequired()])
-    country = TextAreaField('Country', validators=[DataRequired()])
-    submit = SubmitField('Get Weather')
--- weather.py
+++ weather.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template, request, jsonify
-import json
 import requests
+from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
 
-app = Flask(__name__)
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: int
 
-@app.route("/")
-@app.route("/home")
-def home():
-    r = requests.get(
-        'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5802ad3591f068c58a65e854786e5d23')
-    temp = json.loads(r.text)['main']['temp']
+def get_lat_lon(city_name, state_code, country_code, API_key):
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+    data = resp[0]
+    lat, lon = data.get('lat'), data.get('lon')
+    return lat, lon
 
-    return render_template('weather.html', temp=temp)
+def get_current_weather(lat, lon, API_key):
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
+    data = WeatherData(
+        main=resp.get('weather')[0].get('main'),
+        description=resp.get('weather')[0].get('description'),
+        icon=resp.get('weather')[0].get('icon'),
+        temperature=int(resp.get('main').get('temp'))
+    )
+    return data
 
+def main(city_name, state_name, country_name):
+    lat, lon = get_lat_lon(city_name, state_name, country_name, api_key)
+    weather_data = get_current_weather(lat, lon, api_key)
+    return weather_data
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    lat, lon = get_lat_lon('Toronto', 'ON', 'Canada', api_key)
+    print(get_current_weather(lat, lon, api_key))
